13deb7931036c92636323bd6f657842ae3d89db0
ZaKi <390613562@qq.com>
2021-02-19 15:46:42 +0800

    友情链接后台部分完成

--- bigevent-test-fe-static/assets/js/baseApi.js
+++ bigevent-test-fe-static/assets/js/baseApi.js
@@ -5,7 +5,7 @@ $.ajaxPrefilter(function (params) {
     // 如果需要身份认证，则在headers中添加token值，如果没有token，则添加空值
     if (params.url.indexOf('/admin/') !== -1) {
         params.headers = {
-            Authorization: localStorage.getItem('token') || ''
+            Authorization: localStorage.getItem('token')
         }
     }
     // 拦截(有空再写把)
