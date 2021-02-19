a4d35fc95d3f3c1adb80a581acd49740189c7f73
ZaKi <390613562@qq.com>
2021-02-19 19:56:55 +0800

    前台友情链接完成

---
+++ bigevent-custom-static/js/baseApi.js
@@ -0,0 +1,13 @@
+var baseURL = 'http://localhost:8888'
+
+$.ajaxPrefilter(function (params) {
+    params.url = baseURL + params.url
+    // 如果需要身份认证，则在headers中添加token值，如果没有token，则添加空值
+    if (params.url.indexOf('/admin/') !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+    }
+    // 拦截(有空再写把)
+    
+})
